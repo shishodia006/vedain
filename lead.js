efa37d8f1791daa47fc837cc7bce5636781c1ec7
bhanushishodia <mobiweb.developer2@gmail.com>
2026-04-10 11:22:24 +0530

    added node-fetch

--- lead.js
+++ lead.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-// const fetch = require("node-fetch");
+const fetch = require("node-fetch");
 const app = express();
 
 app.use(cors({
